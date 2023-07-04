314ec0b8239920520ad43677b2cd88290a32d726
harikrishnan-sr <30754304+harikrishnan-sr@users.noreply.github.com>
2023-07-04 07:46:09 +0530

    5 files modified, 12 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAAlertComponent } from './success-aalert/success-aalert.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, WarningAlertComponent, SuccessAAlertComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/success-aalert/success-aalert.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-aalert.component.html',
+  styleUrls: ['./success-aalert.component.css'],
+})
+export class SuccessAAlertComponent {}
