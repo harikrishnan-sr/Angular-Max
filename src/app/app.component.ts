432d5e6fb3242784822471ac03753ab899d890c7
harikrishnan-sr <30754304+harikrishnan-sr@users.noreply.github.com>
2023-07-11 13:15:21 +0530

    4 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  eventCount = 0;
-
-  oddComponents: number[] = [];
-  evenComponents: number[] = [];
-
-  onGameEvent() {
-    this.eventCount++;
-    console.log(this.eventCount);
-    if (this.eventCount % 2 === 0) {
-      this.evenComponents.push(this.eventCount);
-    } else {
-      this.oddComponents.push(this.eventCount);
-    }
-  }
+  numbers = [1, 2, 3, 4, 5];
+  onlyOdd = false;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
-import { GameControlComponent } from './game-control/game-control.component';
-import { OddComponent } from './odd/odd.component';
-import { EvenComponent } from './even/even.component';
+import { BasicHighlightDirective } from './basic-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameControlComponent,
-    OddComponent,
-    EvenComponent,
+    BasicHighlightDirective
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
